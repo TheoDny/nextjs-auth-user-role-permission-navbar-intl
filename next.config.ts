36785700d154def388c85283524d49d5f8b6af49
Theo Denoyelle <denoyelletheo@gmail.com>
2025-08-06 19:37:13 +0200

    fix: add prisma plugin to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+const { PrismaPlugin } = require("@prisma/nextjs-monorepo-workaround-plugin")
 import type { NextConfig } from "next"
 import createNextIntlPlugin from "next-intl/plugin"
 
@@ -10,6 +11,13 @@ const nextConfig: NextConfig = {
             bodySizeLimit: "55mb",
         },
         optimizePackageImports: ["@prisma/client"],
+    },
+    webpack: (config, { isServer }) => {
+        if (isServer) {
+            config.plugins = [...config.plugins, new PrismaPlugin()]
+        }
+
+        return config
     },
 }
 
