6cf51009728152d6cc426ddd353d67b5e7ffbdea
Theo Denoyelle <denoyelletheo@gmail.com>
2025-08-07 01:25:26 +0200

    feat: add cron job for database reset and implement authorization in API endpoint

---
+++ app/api/cron/reset-database/route.ts
@@ -0,0 +1,15 @@
+import { exec } from "child_process"
+import { headers } from "next/headers"
+import { NextResponse } from "next/server"
+
+export async function GET() {
+    const headersList = await headers()
+    if (headersList.get("Authorization") !== `Bearer ${process.env.CRON_SECRET}`) {
+        return NextResponse.json({ ok: false, error: "Unauthorized" }, { status: 401 })
+    }
+    //execute a commond line to reset database
+    console.info("Resetting database")
+    const result = await exec("npx prisma migrate reset --force")
+
+    return NextResponse.json({ ok: true })
+}
