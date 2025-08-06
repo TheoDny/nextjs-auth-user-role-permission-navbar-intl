7bf1007caee744ab05e310748f9121577ee3946a
Theo Denoyelle <denoyelletheo@gmail.com>
2025-08-06 18:12:10 +0200

    fix: remove baseURL from auth-client

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -2,7 +2,6 @@ import { inferAdditionalFields } from "better-auth/client/plugins"
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** the base url of the server (optional if you're using the same domain) */
-    baseURL: process.env.NEXT_PUBLIC_APP_URL,
     plugins: [
         inferAdditionalFields({
             user: {
