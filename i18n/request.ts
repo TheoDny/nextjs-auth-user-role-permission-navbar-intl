f111adf679083e8f9b42deee998b36d9b3984586
Theo Denoyelle <denoyelletheo@gmail.com>
2026-03-20 23:46:04 +0100

    feat: improve locale handling in try catch to avoid any evenual error from that part

--- i18n/request.ts
+++ i18n/request.ts
@@ -4,21 +4,29 @@ import { cookies, headers } from "next/headers"
 export const IMPLEMENTED_LOCALE = ["en", "fr"]
 
 export default getRequestConfig(async () => {
-    let locale =
-        (await cookies()).get("NEXT_LOCALE")?.value || (navigator.language.split("-")[0] === "fr" ? "fr" : "en")
+    try {
+        let locale =
+            (await cookies()).get("NEXT_LOCALE")?.value ||
+            (navigator.language.split("-")[0] === "fr" ? "fr" : "en")
 
-    if (!IMPLEMENTED_LOCALE.includes(locale)) {
-        const h = (await headers()).get("accept-language")
-        let l
-        if (h && h.length > 2) {
-            l = h[0] + h[1]
+        if (!IMPLEMENTED_LOCALE.includes(locale)) {
+            const h = (await headers()).get("accept-language")
+            let l
+            if (h && h.length > 2) {
+                l = h[0] + h[1]
+            }
+
+            locale = l || "en"
         }
 
-        locale = l || "en"
-    }
-
-    return {
-        locale,
-        messages: (await import(`./messages/${locale}.json`)).default,
+        return {
+            locale,
+            messages: (await import(`./messages/${locale}.json`)).default,
+        }
+    } catch (error) {
+        return {
+            locale: "en",
+            messages: (await import(`./messages/en.json`)).default,
+        }
     }
 })
